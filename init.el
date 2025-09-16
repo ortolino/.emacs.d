628d7a812ced8fd43ab829be43afb59ad583357e
Lorenzo <lorenzo.padrini@epfl.ch>
2025-09-16 14:54:26 +0200

    added main init file with theme and magit

---
+++ init.el
@@ -0,0 +1,21 @@
+;; This is my emacs init file. I hope I can keep it as long as I can and configure it to my likings
+(require 'use-package)
+;; Setup visual theme
+(load-theme 'modus-vivendi)
+
+
+;; Downloading some cool utils
+(use-package magit
+  :ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
