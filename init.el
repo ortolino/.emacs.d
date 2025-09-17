1f72e5dcd280b94dd922cbfc22846eb14ae760e9
Lorenzo <lorenzo.padrini@epfl.ch>
2025-09-17 13:19:51 +0200

    Added basic lean functionality

--- init.el
+++ init.el
@@ -1,18 +1,53 @@
 ;; This is my emacs init file. I hope I can keep it as long as I can and configure it to my likings
+(require 'package)
 (require 'use-package)
+;; Add MELPA repository for additional packages
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+
+;; INTERFACE: change the visual style and the management of the interface
+
 ;; Setup visual theme
 (load-theme 'modus-vivendi)
 
+;; PROGRAMMING: setup all main programming languages
 
-;; Downloading some cool utils
+;; LSP MODE: util that is used for all programming languages
+(use-package lsp-mode
+  :ensure t)
+
+;; Git: install magit for git management
 (use-package magit
   :ensure t)
+(require 'magit)
+
+;; Lean: Configuration for lean
+(use-package dash
+  :ensure t) ;; install dependencies
+
+(use-package lean4-mode
+  :commands lean4-mode
+  :vc (:url "https://github.com/leanprover-community/lean4-mode.git"
+       :rev
+       :last-release
+       :requires dash lsp-mode magit-section
+       ))
+
+
+;; VARIABLES AND FACES: added automatically by Emacs
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
+ '(package-selected-packages '(dash: lean4-mode lsp-mode magit t2))
+ '(package-vc-selected-packages
+   '((lean4-mode :url
+		 "https://github.com/leanprover-community/lean4-mode.git")))
+ '(safe-local-variable-values
+   '((eval add-hook 'before-save-hook #'whitespace-cleanup nil t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
