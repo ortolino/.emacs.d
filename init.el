6edb82de74bd2b7a5861f6fcc4413eb35bcb9e7d
lore <lorenzo.padrini@epfl.ch>
2026-06-07 14:54:54 +0200

    feat: added multiple programming language features

--- init.el
+++ init.el
@@ -5,10 +5,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 ;;(package-refresh-contents)
-
-
 ;; INTERFACE: change the visual style and the management of the interface
-
 ;; Setup visual theme
 (load-theme 'modus-operandi)
 
@@ -33,14 +30,54 @@
 ;; PROGRAMMING: setup all main programming languages
 
 ;; Set spaces instead of tabs:
-(setq-default indent-tabs-mode t)
-(setq-default tab-width 4) ; Assuming you want your tabs to be four spaces wide
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+;; Syntax Highlighting with flycheck
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+;; Auto completion with company-mode
+(use-package company
+  :ensure t
+  :hook (after-init-hook global-company-mode))
 
 ;; Git: install magit for git management
 (use-package magit
   :ensure t)
 (require 'magit)
+	    
+;; Markdown writing
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
 
+;; Rocq development
+(use-package proof-general
+  :ensure t)
+
+(use-package company-coq
+  :ensure t
+  :hook (cook-mode-hook . company-coq-mode))
+
+
+
+;; Lean development
+(setq exec-path (append exec-path '("~/.elan/env"))) ;; Add local bin for emac functions
+(use-package nael
+  :ensure t
+  :hook (nael-mode . abbrev-mode) (nael-mode . eglot-ensure) (nael-mode . (lambda () (prettify-symbols-mode))))
+
+;; Rust development
+(use-package rust-mode
+  :ensure t
+  :hook ((rust-mode . eglot-ensure) (rust-mode . (lambda () (prettify-symbols-mode))))
+  :init (setq rust-mode-treesitter-derive t))
 
 
 ;; Haskell: add haskell mode to code haskell code
@@ -63,7 +100,6 @@
   (eglot-confirm-server-initiated-edits nil)  
   )
 
-;; Variables AND FACES: added automatically by Emacs
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -71,16 +107,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(eglot-confirm-server-edits nil nil nil "Customized with use-package eglot")
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((lean4-mode :url
-				 "https://github.com/leanprover-community/lean4-mode.git")))
- '(safe-local-variable-values
-   '((eval add-hook 'before-save-hook #'whitespace-cleanup nil t))))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
