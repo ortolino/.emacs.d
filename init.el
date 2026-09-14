74d36be1709d2a90d306478fcae412a5466a1687
Lorenzo <lorenzo.padrini@epfl.ch>
2026-09-14 12:53:57 +0200

    feat: added markdown to org export list

--- init.el
+++ init.el
@@ -127,10 +127,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(eglot-confirm-server-edits nil nil nil "Customized with use-package eglot")
- '(package-selected-packages
-   '(company-coq consult flycheck haskell-mode magit marginalia
-                 markdown-mode nael proof-general rust-mode vertico
-                 web-mode)))
+ '(org-export-backends '(ascii html icalendar latex md odt))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
